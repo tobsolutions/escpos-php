5fbf8747f93b1085b0104c8cf761b9eee09a9bfe
tobsolutions <tobi@tobsolutions.de>
2024-06-26 08:39:48 +0200

    Printconnector und Print-Design, JSON Import,

--- php/print-network.php
+++ php/print-network.php
@@ -2,26 +2,29 @@
 /* Change to the correct path if you copy this example! */
 require __DIR__ . '/../vendor/autoload.php';
 use Mike42\Escpos\Printer;
+use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 
 /* Most printers are open on port 9100, so you just need to know the IP 
  * address of your receipt printer, and then fsockopen() it on that port.
  */
 try {
-    $connector = new NetworkPrintConnector("192.168.1.25", 9100);
+    $connector = new NetworkPrintConnector("192.168.1.25", 9100, 5);
     
     /* Print a "Hello world" receipt" */
     $printer = new Printer($connector);
     $printer -> setJustification(Printer::JUSTIFY_CENTER);
+    $printer -> bitImage(EscposImage::load("logo.png", Printer::IMG_DOUBLE_WIDTH));    
+    $printer -> feed();
     $printer -> setTextSize(1, 1);
     $printer -> text("Musikvereinigung Tiefenbach\n");
     $printer -> text("Brotzeitabend 2024\n");
     $printer -> feed();
-    $printer -> setTextSize(3, 3);
-    $printer -> text("Wurstsalat\n");
-    $printer -> feed();
     $printer -> setTextSize(2, 2);
-    $printer -> text("3,50 EUR\n");
+    $printer -> text($_GET["artikel"]."\n");
+    $printer -> feed();
+    $printer -> setTextSize(2, 1);
+    $printer -> text($_GET["preis"]."\n");
     $printer -> feed();
     $printer -> setTextSize(1, 1);
     $printer -> text(date("d.m.Y H:i:s")."\n");
